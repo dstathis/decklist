ad18c3097d85d3d2ce05316bd5b1bbe1bbc56b89
Dylan Stephano-Shachter <dylan@theone.ninja>
2019-10-10 23:29:36 -0400

    script to generate set code dict

---
+++ generate_data.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import scrython
+
+def generate_data():
+    with open('decklist/setcodes.py', 'w') as setfile:
+        setfile.write('setcodes = {\n')
+        sets = scrython.sets.Sets()
+        for expansion in sets.data():
+            setfile.write(f'\"{expansion["code"]}\": \"{expansion["name"]}\",\n')
+        setfile.write('}')
+
+
+def main():
+    generate_data()
+
+
+if __name__ == '__main__':
+    main()
